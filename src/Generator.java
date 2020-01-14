eab7b97a6d3534312b651ba53e4eb1b2687cd206
Marcin Tomasz Dydo <m.dydo@stud.elka.pw.edu.pl>
2020-01-14 20:32:17 +0100

    Major Update Generator.java

--- src/Generator.java
+++ src/Generator.java
@@ -2,26 +2,25 @@ import java.util.Random;
 
 public class Generator {
     private final int[][] MAP; //cannot point to any other array
+    Random rand = new Random(); //useful random object :)
 
     public int[][] getMAP(int ratio) {
+        makeMap(ratio);
         return MAP;
     }
 
     /**
      * Generates map that is able to be saved
      * in a CSV file
-     *
      * @param x, y
      */
     public Generator(int x, int y) {
         MAP = new int[x][y];
-         makeMap(3);
     }
 
     /**
      * Fills the map with random structure
-     * and checks if no rules has been broken
-     * need to add: solver check
+     * and solver-checks it
      */
     private void makeMap(int ratio) {
         for (int i = 0; i < MAP.length; i++) {
@@ -29,25 +28,37 @@ public class Generator {
                 MAP[i][j] = assignRandom(ratio); //ratio blacks:whites - 1:n
             }
         }
-        while(!checkForBlank()){
+        while(!checkForDark()){
             solveCheck();//solver method to check and finish the map
+        }
+        for (int i = 0; i < MAP.length; i++) {
+            for (int j = 0; j < MAP[0].length; j++) {
+                if(MAP[i][j]==6)
+                MAP[i][j] = assignNumber(8,i,j); //seek for bulbs
+            }
+        }
+        for (int i = 0; i < MAP.length; i++) {
+            for (int j = 0; j < MAP[0].length; j++) {
+                if(MAP[i][j]==7 || MAP[i][j]==8) MAP[i][j] = 0; //wipes out Bulb and Lit tiles
+            }
         }
     }
     /**
      * assigns random values depending on ratios
-     * @return
+     * @return Dark or Black
      */
     private int assignRandom(int ratio) {
-        Random rand = new Random();
         if (rand.nextInt(ratio) > 0) return 0;
         else return 6;
     }
 
-    private boolean checkForBlank(){
-        for (int[] i:MAP
-        ) {
-            for (int j:i
-            ) {
+    /**
+     * checks if any Dark space left
+     * @return true if not
+     */
+    private boolean checkForDark(){
+        for (int[] i:MAP) {
+            for (int j:i) {
                 if (j==0)return false;
             }
         }
@@ -57,27 +68,43 @@ public class Generator {
      * solver made only for generator
      */
     private void solveCheck(){
-        Random rand = new Random();
         int x = rand.nextInt(MAP.length);
         int y = rand.nextInt(MAP[0].length);
         if(MAP[x][y] == 0){
-                    MAP[x][y]=8;
-                    Expansion.expand(MAP,7 ,x,y);
-
+            Expansion.expand(MAP,7 ,x,y);
         }
+    }
+    /**
+     * counts neighbours of value 8 (bulbs)
+     * near field [i,j]
+     * @param i x parameter
+     * @param j y parameter
+     * @param tile tile to check (default bulb)
+     * @return number of neighbours therefore value of tile
+     */
+    private int assignNumber(int tile, int i, int j){
+        int counter=0;
+        try{
+            if(MAP[i-1][j] == tile) counter++;
+        }catch (IndexOutOfBoundsException ignored){}
+        try{
+            if(MAP[i][j-1] == tile) counter++;
+        }catch (IndexOutOfBoundsException ignored){}
+        try{
+            if(MAP[i+1][j] == tile) counter++;
+        }catch (IndexOutOfBoundsException ignored){}
+        try{
+            if(MAP[i][j+1] == tile) counter++;
+        }catch (IndexOutOfBoundsException ignored){}
+        if(rand.nextInt(2)>0){
+            if(counter>0)return counter;
+            else return 5;
+        }else return 6;
     }
 
     public static void main(String[] args) {
         Akari a=new Akari(1,1);
         Generator gen = new Generator(10,10);
-        a.swap(gen.MAP);
-        for (int[] i:gen.MAP
-             ) {
-            for (int j:i
-                 ) {
-                System.out.print(j + ",");
-            }
-            System.out.println();
-        }
+        a.swap(gen.getMAP(4));
     }
 }
